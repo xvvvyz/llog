090141808a18197c6bed36e732de04d70c3fb0ee
cade <git@cade.me>
2025-05-16 19:50:26 -0600

    remove console.logs in production

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ module.exports = function (api) {
   api.cache(true);
 
   return {
+    env: {
+      production: {
+        plugins: ['transform-remove-console'],
+      },
+    },
     plugins: [
       '@babel/plugin-proposal-export-namespace-from',
       'react-native-reanimated/plugin',
